2536058a450039394e8cd90c9eb7b36f5d1e3c5e
matyjb <2fishx@gmail.com>
2021-01-22 19:23:55 +0100

    bugfixes to encoding

--- LZ77.py
+++ LZ77.py
@@ -11,23 +11,27 @@ LOOK_AHEAD_BUFFER_SIZE = 6
 
 def encode(stream):
   index = 0
-  matching = 0
-  offset = 0
   while(len(stream) > index):
     matching = 0
     offset = 0
+    lastchar = ''
     for i in range(LOOK_AHEAD_BUFFER_SIZE):
+      if(index+i+1 > len(stream)):
+        break
       schar = stream[index:index+i+1]
-      buffer_start_index = 0 if index-DICT_BUFFER_SIZE < 0 else index-DICT_BUFFER_SIZE
+      buffer_start_index = 0 if index+i-DICT_BUFFER_SIZE < 0 else index+i-DICT_BUFFER_SIZE
       v = stream.find(schar,buffer_start_index,index+i)
+      # print(index,i,schar,"szukam w",stream[buffer_start_index:index+i])
       
       if(v == -1):
-        yield (offset, matching, schar[-1])
+        lastchar = schar[-1]
         break
       else:
         matching += 1
         offset = index-v
-
+    
+    print((offset, matching, lastchar))
+    yield (offset, matching, lastchar)
     index += matching+1
 
 def decode(stream):
@@ -46,11 +50,11 @@ def decode(stream):
 def main():
   file = open("test.txt")
   text = file.read()
-  print(text)
-  e = list(encode(text))
-  print(e)
-  d = decode(e)
-  print(d)
+  # print(text)
+  # for e in encode(text):
+  #   print(e)
+  d = decode(list(encode(text)))
+  # print(d)
   print(text == d)
   pass
 
