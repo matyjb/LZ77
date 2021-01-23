c79dba52175a041c7d4c33cd301340aee264b2d8
matyjb <2fishx@gmail.com>
2021-01-23 14:27:35 +0100

    fix to test.sh

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ python LZ77.py -d lz77/icing.wav.lz77 decoded/icing.wav.lz77.wav
 python LZ77.py -d lz77/song.wav.lz77 decoded/song.wav.lz77.wav
 python LZ77.py -d lz77/img1.bmp.lz77 decoded/img1.bmp.lz77.bmp
 python LZ77.py -d lz77/piesel.jpg.lz77 decoded/piesel.jpg.lz77.jpg
-python LZ77.py -d lz77/tenor.png.lz77 decoded/tenor.png.lz77.jpg
-python LZ77.py -d lz77/ogromny.png.lz77 decoded/ogromny.png.lz77.jpg
+python LZ77.py -d lz77/tenor.png.lz77 decoded/tenor.png.lz77.png
+python LZ77.py -d lz77/ogromny.png.lz77 decoded/ogromny.png.lz77.png
 python LZ77.py -d lz77/lorem.txt.lz77 decoded/lorem.txt.lz77.txt
 python LZ77.py -d lz77/pantadeusz.txt.lz77 decoded/pantadeusz.txt.lz77.txt
